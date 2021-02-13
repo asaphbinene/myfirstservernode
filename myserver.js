d881ac6900987047c06a641d2fd4649f6731900b
KAT Binene <asaphbinene@outlook.com>
2021-02-13 23:16:57 +0200

    Adding the request package to the server

--- myserver.js
+++ myserver.js
@@ -1,4 +1,13 @@
 require("http").createServer((ktbRequest, ktbResponse) => 
 {
-    ktbResponse.end("Welcome to Katbin.com server 001");
+    const ktbRequestModule = require("request");
+    ktbRequestModule(
+        "http://worldtimeapi.org/api/timezone/America/New_York",
+        function(ktbErr, ktbReply, ktbContent) 
+        {
+            ktbResponse.end(`<h1>Welcome to Katbin.com server 001:</h1> 
+            <p> ${ktbContent} </p>`);
+        }
+    );
+    
 }).listen(7724);
